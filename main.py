f86f0749db1d0dcf79a20d48472577cbb778f438
alien204 <varunpriyesh2014@gmail.com>
2023-01-24 19:30:06 +0530

    added notebook

---
+++ main.py
@@ -0,0 +1,16 @@
+from djitellopy import Tello
+
+tello = Tello()
+
+tello.connect(self)
+#tello.takeoff()
+
+#tello.move_left(100)
+#tello.rotate_counter_clockwise(90)
+#tello.move_forward(100)
+
+#t=input("ENTER WHAT YOU WANT TO DO")
+#if t=="1":
+ #   tello.land()
+
+tello.get_battery(self)
